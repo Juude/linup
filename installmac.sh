50baf38d55980eea8a608e616b09fd716dc9c6de
J.D.Song <juude.song@gmail.com>
2016-04-06 17:44:15 +0800

    update tools

---
+++ installmac.sh
@@ -0,0 +1,6 @@
+#configure mac development environment
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install the_silver_searcher
+brew install git-cola
+brew install fswatch
